c9a44b469da85e085187f55e4bfb782f77b81de1
J-hi <juhi.gupta@avantika.edu.in>
2025-03-10 20:32:24 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const App = () => {
   return (
-      <BrowserRouter>
+      
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/favsec" element={<Favsec/>} />
       </Routes>
-    </BrowserRouter>
+    
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
+import { HashRouter } from 'react-router-dom';
 
 
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/React-demo-deployment-activity/"> 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
